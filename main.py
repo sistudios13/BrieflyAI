82097d6dd504a271e0f55ba47292f944b6b5c534
Westr <si.studios13@gmail.com>
2025-02-07 10:24:43 -0500

    Fully working
    Uses NewsAPI to fetch headline URLS
    Scrapes sites with newspaper
    summarizes twice with Bart AI

--- main.py
+++ main.py
@@ -2,11 +2,10 @@ import requests
 from dotenv import load_dotenv
 import os
 from transformers import pipeline
+from newspaper import Article
 
 
 load_dotenv()
-
-
 API_KEY = os.getenv("API_KEY")
 url = "https://newsapi.org/v2/top-headlines?country=us&apiKey=" + API_KEY
 
@@ -16,22 +15,49 @@ def get_news():
     articles = data.get('articles')
     news_list = []
     for item in articles:
-        news_list.append(
-            {
-            "title" : item['title'],
-            "content" : item['content']
-            }
-        )
+        if item['url'] is not None:
+            news_list.append(
+                {
+                "url" : item['url']
+                }
+            )
 
     return news_list
 
-news = get_news()[0:3]
-sentence = [i['content'] for i in news]
 
-sentences = ' '.join(sentence[:2])
+def summarize_news(article, length):
+    if article == "":
+        return
+    summarizer = pipeline(task='summarization', model='facebook/bart-large-cnn')
+    summary = summarizer(article, max_length=length, min_length=80, do_sample=True, temperature=0.7)
+    return summary
 
-print(sentences)
-summarizer = pipeline(task='summarization')
 
-summary = summarizer(sentences, max_length=100, min_length=20, do_sample=False)
-print(summary[0]["summary_text"])
+def scrape_news(a):
+    article = Article(a['url'])
+    try:
+        article.download()
+        article.parse()
+        summary = summarize_news(article.text, 100)
+        return summary
+    except:
+        pass
+
+
+def summarize_again(count):
+    # Summarize all the summaries
+    news = get_news()[:count]
+    summary_list = []
+    for article in news:
+        summary = scrape_news(article)
+        if summary is not None:
+            summary_list.append(summary[0]['summary_text'])
+
+    summaries = ' '.join(summary_list)
+    output = summarize_news(summaries, 160)
+    print(output)
+
+
+
+
+summarize_again(6)
